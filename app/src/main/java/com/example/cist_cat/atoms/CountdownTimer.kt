b07a8616ddbdc7462b66403c3d924974bef85e4f
ItisNoMatter <702sunnylife@gmail.com>
2023-03-19 16:01:24 +0900

    副作用の例としてMyCountUpTimerを追加

--- app/src/main/java/com/example/cist_cat/atoms/CountdownTimer.kt
+++ app/src/main/java/com/example/cist_cat/atoms/CountdownTimer.kt
@@ -1,21 +1,22 @@
 package com.example.cist_cat.atoms
 
 import android.os.Build
-import android.os.CountDownTimer
-import android.widget.TextClock
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
 import java.time.Duration
 import java.time.LocalTime
-import java.util.Date
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun CountDownTimer(currentTime:LocalTime,nextBusTime:LocalTime){
-    Column() {
+    Column {
         val countDown = Duration.between(currentTime,nextBusTime)
         val countDownMinute = countDown.toMinutes()
         val countDownSeconds = (countDown.toMillis() / 1000 ) - countDownMinute*60
@@ -24,4 +25,34 @@ fun CountDownTimer(currentTime:LocalTime,nextBusTime:LocalTime){
         Text(text = "$countDownMinute")
         Text(text= "$countDownSeconds")
     }
+}
+
+@Composable
+fun MyCountUpTimer(){
+    var count:Int by remember { mutableStateOf(0) }
+    var isRunning by remember { mutableStateOf(false) }
+    if (isRunning){
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1000)
+                count++
+            }
+        }
+    }
+    Column {
+        Text(text = count.toString(), style = MaterialTheme.typography.h1)
+        Button(
+            onClick ={isRunning = !isRunning},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            if(isRunning) Text("pause")  else Text("start to count up")
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun Prev(){
+    MyCountUpTimer()
 }
