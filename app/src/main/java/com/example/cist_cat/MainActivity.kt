14b05d534d10d3e869d3d9e0c1989115501d0bbf
ItisNoMatter <702sunnylife@gmail.com>
2023-03-21 20:45:54 +0900

    MainActivity.ktを一旦クリーンな状態に

--- app/src/main/java/com/example/cist_cat/MainActivity.kt
+++ app/src/main/java/com/example/cist_cat/MainActivity.kt
@@ -7,17 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import com.example.cist_cat.ui.theme.CISTCaTTheme
-import com.example.cist_cat.atoms.NeumorphismSample
-import com.example.cist_cat.atoms.CountDownTimer
-import java.time.LocalTime
-import java.util.Date
-
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -28,10 +19,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = Color.White,
                 ) {
-                    val time by remember { mutableStateOf(System.currentTimeMillis()) }
-                    val currentTime = LocalTime.now()
-                    val nextBusTime = LocalTime.of(23,59) //TODO Get Actual value
-                    CountDownTimer(currentTime = currentTime, nextBusTime = nextBusTime)
+                    Text("hello cist")
                 }
             }
         }
