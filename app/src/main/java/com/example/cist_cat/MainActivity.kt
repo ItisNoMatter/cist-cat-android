d36b604dacaa03b857007ad03dd345a31256b5c7
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-03-23 18:02:28 +0900

    Direction-Toggleの実装

--- app/src/main/java/com/example/cist_cat/MainActivity.kt
+++ app/src/main/java/com/example/cist_cat/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cist_cat.ui.theme.CISTCaTTheme
--- app/src/main/java/com/example/cist_cat/atoms/directionToggle.kt
+++ app/src/main/java/com/example/cist_cat/atoms/directionToggle.kt
@@ -1,42 +1,66 @@
 package com.example.cist_cat.atoms
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Switch
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cist_cat.ui.theme.CISTCaTTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CISTCaTTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    DirectionToggle()
-                }
+@Composable
+private fun DirectionToggle(){
+    val outwardTrip = remember { mutableStateOf(true) }
+    if(outwardTrip.value){
+        Row{
+            Button(
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = androidx.compose.ui.graphics.Color(0xFFFFA59F),
+                    contentColor = androidx.compose.ui.graphics.Color.White,
+                    disabledContentColor = androidx.compose.ui.graphics.Color.LightGray
+                ),
+                onClick = {}
+            ){
+                Text("往路")
+            }
+            Button(
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = androidx.compose.ui.graphics.Color.White,
+                    contentColor = androidx.compose.ui.graphics.Color.Gray,
+                    disabledContentColor = androidx.compose.ui.graphics.Color.LightGray
+                ),
+                onClick = { outwardTrip.value = !outwardTrip.value }
+            ){
+                Text("復路")
+            }
+        }
+    }else{
+        Row{
+            Button(
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = androidx.compose.ui.graphics.Color.White,
+                    contentColor = androidx.compose.ui.graphics.Color.Gray,
+                    disabledContentColor = androidx.compose.ui.graphics.Color.LightGray
+                ),
+                onClick = { outwardTrip.value = !outwardTrip.value }
+            ){
+                Text("往路")
+            }
+            Button(
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = androidx.compose.ui.graphics.Color(0xFFFFA59F),
+                    contentColor = androidx.compose.ui.graphics.Color.White,
+                    disabledContentColor = androidx.compose.ui.graphics.Color.LightGray
+                ),
+                onClick = {}
+            ){
+                Text("復路")
             }
         }
     }
-}
-@Composable
-private fun DirectionToggle(){
-    val checked = remember { mutableStateOf(true) }
-    Switch(
-        checked = checked.value,
-        onCheckedChange = { checked.value = it },
-    )
+
 }
 
 @Preview(showBackground = true, widthDp = 320)
