4088319ea2abdfcda725746eeed57e314390485f
ItisNoMatter <702sunnylife@gmail.com>
2023-03-31 23:32:21 +0900

    親からmodifier渡せるように変更

--- app/src/main/java/com/example/cist_cat/atoms/Logo.kt
+++ app/src/main/java/com/example/cist_cat/atoms/Logo.kt
@@ -1,11 +1,15 @@
 package com.example.cist_cat.atoms
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.example.cist_cat.R
 
 
 @Composable
-fun Logo(){
-    Image(painter = painterResource(R.drawable.logo), contentDescription ="logo" )
+fun Logo(modifier: Modifier = Modifier,){
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.logo),
+        contentDescription ="logo" )
 }
